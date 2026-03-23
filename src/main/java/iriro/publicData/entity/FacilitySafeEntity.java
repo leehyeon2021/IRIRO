4474676c4cd3407148261be0b16d620114afcd10
leehyeon2021 <leehyeon2021@gmail.com>
2026-03-23 15:30:38 +0900

    260323 facility_safe entity 테이블 생성 확인 완료

--- src/main/java/iriro/publicData/entity/FacilitySafeEntity.java
+++ src/main/java/iriro/publicData/entity/FacilitySafeEntity.java
@@ -1,4 +1,18 @@
 package iriro.publicData.entity;
 
+import jakarta.persistence.*;
+
+@Entity @Table(name = "facility_safe")
 public class FacilitySafeEntity {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer fac_id;
+    private String fac_type;
+    private String fac_sgg;
+    private String fac_name;
+    private String fac_add;
+    private Double fac_lat;
+    private Double fac_lng;
+    private Integer fac_count;
+    private String fac_use;
+    private String fac_tel;
 }
