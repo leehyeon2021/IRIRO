d2482a4d47fb9ee84588c3a4e62dbf2cc0469e0d
so0hub <so0mile02@gmail.com>
2026-03-24 00:05:14 +0900

    박소영 12:05

--- src/main/java/iriro/community/controller/BoardController.java
+++ src/main/java/iriro/community/controller/BoardController.java
@@ -1,11 +1,27 @@
 package iriro.community.controller;
 
+import iriro.community.dto.BoardDto;
+import iriro.community.service.BoardService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/board")
+@RequestMapping("/board")
 public class BoardController {
+
+    @Autowired
+    private BoardService boardService;
+
+    // 1. 리뷰 등록
+    // http://localhost:8080/board/rbwrite
+    // {"" : "" , "" : "" }
+    @PostMapping("rbwrite")
+    public boolean rbAdd(BoardDto boardDto){
+        boolean result = boardService.rvAdd(boardDto);
+        return result;
+    }
 }
