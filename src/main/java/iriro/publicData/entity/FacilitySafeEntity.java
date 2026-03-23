ec5940bbf2f5ad19358c27d4251453fdd3f5297a
leehyeon2021 <leehyeon2021@gmail.com>
2026-03-23 15:38:18 +0900

    260323 facility_safe entity 테이블 생성 확인 완료

--- src/main/java/iriro/publicData/entity/FacilitySafeEntity.java
+++ src/main/java/iriro/publicData/entity/FacilitySafeEntity.java
@@ -1,18 +1,42 @@
 package iriro.publicData.entity;
 
 import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Data;
 
 @Entity @Table(name = "facility_safe")
+@Data @Builder
 public class FacilitySafeEntity {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer fac_id;
-    private String fac_type;
-    private String fac_sgg;
-    private String fac_name;
-    private String fac_add;
-    private Double fac_lat;
-    private Double fac_lng;
-    private Integer fac_count;
-    private String fac_use;
-    private String fac_tel;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "fac_id")
+    private Integer facId;
+
+    @Column(name = "fac_type", nullable = false, length = 50)
+    private String facType;
+
+    @Column(name = "fac_sgg", length = 50)
+    private String facSgg;
+
+    @Column(name = "fac_name", length = 50)
+    private String facName;
+
+    @Column(name = "fac_add", length = 300)
+    private String facAdd;
+
+    @Column(name = "fac_lat")
+    private Double facLat;
+
+    @Column(name = "fac_lng")
+    private Double facLng;
+
+    @Column(name = "fac_count")
+    private Integer facCount;
+
+    @Column(name = "fac_use", length = 10)
+    private String facUse;
+
+    @Column(name = "fac_tel", length = 30)
+    private String facTel;
 }
+
