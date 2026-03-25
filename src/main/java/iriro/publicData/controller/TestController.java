a1f2a335044e215521aaec0c789c2b81473d4033
bm1611 <bm1611@naver.com>
2026-03-25 15:11:21 +0900

    주석처리

--- src/main/java/iriro/publicData/controller/TestController.java
+++ src/main/java/iriro/publicData/controller/TestController.java
@@ -16,8 +16,8 @@ public class TestController {
     // 저장 기능을 테스트하기 위한 컨트롤러입니다.
 
     // 범죄자거주도로명주소
-    @GetMapping("/test1")
-    public ResponseEntity<?> test1(){
-        return ResponseEntity.ok(cf.fetchCrimeRoad());
-    }
+//    @GetMapping("/test1")
+//    public ResponseEntity<?> test1(){
+//        return ResponseEntity.ok(cf.fetchCrimeRoad());
+//    }
 }
