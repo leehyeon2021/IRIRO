014cd9c56519b9010eeae479759160d1795c3a3a
so0hub <so0mile02@gmail.com>
2026-03-26 17:45:38 +0900

    [토큰] 댓글 등록

--- src/main/java/iriro/community/controller/ReplyController.java
+++ src/main/java/iriro/community/controller/ReplyController.java
@@ -3,6 +3,7 @@ package iriro.community.controller;
 import iriro.community.dto.ReplyDto;
 import iriro.community.entity.ReplyEntity;
 import iriro.community.repository.ReplyRepository;
+import iriro.community.service.JWTService;
 import iriro.community.service.ReplyService;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
@@ -21,14 +22,22 @@ public class ReplyController {
 
     @Autowired
     private final ReplyService replyService;
+    private final JWTService jwtService;
 
     // 1. 댓글 등록
     // http://localhost:8080/reply/rpwrite
     // { "replyContent" : "박진감보고싶습니감ㅠㅠ" , "boardId" : 2 }
     @PostMapping("/rpwrite")
-    public ResponseEntity<?> rpAdd(@RequestBody ReplyDto replyDto, HttpSession session) {
-        Object object = session.getAttribute("email");
-        String loginEmail = (String) object;
+    public ResponseEntity<?> rpAdd(@RequestBody ReplyDto replyDto, @RequestHeader("Authorization")String token) {
+
+        // 기본값을 null로 시작(비회원 상태)
+
+        String loginEmail = null;
+
+        if(token != null && token.startsWith("Bearer")) {
+            String realToken = token.substring(7);
+            loginEmail = jwtService.getClaim(realToken);
+        }
         boolean result = replyService.rpAdd(replyDto, loginEmail);
         return ResponseEntity.ok(result);
     }
