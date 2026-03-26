c5aa8452220f5baa42858b65ef2487faf207192f
leehyeon2021 <leehyeon2021@gmail.com>
2026-03-26 14:52:39 +0900

    260326 안전 - 지오코딩 수정

--- src/main/java/iriro/publicData/service/FacilitySafeFetchService.java
+++ src/main/java/iriro/publicData/service/FacilitySafeFetchService.java
@@ -131,7 +131,7 @@ public class FacilitySafeFetchService {
 
                     // x="0" 이면 지오코딩 (x없음y도없음)
                     if (xStr == null || xStr.equals("0")) {
-                        double[] coords = gs.getCoords(rnAdres, "F00");
+                        double[] coords = gs.getCoords(rnAdres);
                         if(coords == null)continue;
                         lat = coords[0];
                         lng = coords[1];
