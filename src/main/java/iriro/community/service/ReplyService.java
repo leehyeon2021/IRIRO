e29ca18609caa1b520a6752dbed91f23d6f02ffd
so0hub <so0mile02@gmail.com>
2026-03-26 14:12:26 +0900

    [세션 연동] 댓글 등록

--- src/main/java/iriro/community/service/ReplyService.java
+++ src/main/java/iriro/community/service/ReplyService.java
@@ -1,27 +1,63 @@
 package iriro.community.service;
 
 import iriro.community.dto.ReplyDto;
+import iriro.community.entity.BoardEntity;
 import iriro.community.entity.ReplyEntity;
+import iriro.community.entity.UserEntity;
+import iriro.community.repository.BoardRepository;
 import iriro.community.repository.ReplyRepository;
+import iriro.community.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class ReplyService {
     private final ReplyRepository replyRepository;
+    private final UserRepository userRepository;
+    private final BoardRepository boardRepository;
 
     // 1. 댓글 등록
-    public boolean rpAdd(ReplyDto replyDto){
+    public boolean rpAdd(ReplyDto replyDto,String loginEmail){
         // 1] dto --> entity 변환
-        ReplyEntity replyEntity = replyDto.toEntity();
-        // 2] JPA save 이용하여 insert 하기
-        ReplyEntity saved = replyRepository.save(replyEntity);
-        // 3] save 결과에 pk 여부 성공판단
-        if(saved.getReplyId() >= 1) return true;
+        ReplyEntity saveReplyEntity = replyDto.toEntity();
+        // ********** 저장하기 전에 FK 대입하기 , Fk의 엔티티를 찾아서 대입 **************
+
+        UserEntity userEntity;
+
+        // 비회원일 때
+        if(loginEmail==null){
+        Optional<UserEntity> entityOptional1 = userRepository.findById(1);
+        userEntity= entityOptional1.get();
+        }
+        else{
+            // 회원일 때
+            // 현재 로그인 중인 email로 엔티티 찾기
+            Optional<UserEntity> entityOptional = userRepository.findByEmail(loginEmail);
+            if(entityOptional.isPresent()) {
+                userEntity = entityOptional.get();
+            } else{
+                return false; // 세션은 있는데 DB에 유저가 없다면 실패
+            }
+        }
+        // 찾은 유저를 댓글에 대입
+        saveReplyEntity.setUserEntity(userEntity);
+
+        // * 어떤 글에 다는 댓글인지
+        Optional<BoardEntity> boardOptional = boardRepository.findById(replyDto.getBoardId());
+
+        ReplyEntity savedEntity  = replyRepository.save(saveReplyEntity);
+        if(boardOptional.isPresent()){
+            saveReplyEntity.setBoardEntity(boardOptional.get());
+
+            replyRepository.save(saveReplyEntity);
+            return true;
+        }
         return false;
     }
 
