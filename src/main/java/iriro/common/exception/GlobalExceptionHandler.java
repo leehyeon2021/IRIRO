b97a2648fabe104e75f8818cdf95d58f245c9056
bm1611 <bm1611@naver.com>
2026-03-30 17:30:52 +0900

    로그저장 예외처리 추가

--- src/main/java/iriro/common/exception/GlobalExceptionHandler.java
+++ src/main/java/iriro/common/exception/GlobalExceptionHandler.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @Slf4j // 로그 객체를 자동으로 만들어 줌.
 public class GlobalExceptionHandler {
 
+    // 외부 TmapAPI 호출 실패 == 경로 조회 실패
     @ExceptionHandler(ExternalApiException.class)
     public ResponseEntity<String> handleExternalApiException(ExternalApiException e) {
 
@@ -18,5 +19,26 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(HttpStatus.BAD_GATEWAY)
                 .body("경로 조회에 실패했습니다.");
+    }
+
+    // 로그 저장 실패
+    @ExceptionHandler(LogSaveException.class)
+    public ResponseEntity<String> handleLogSaveException(LogSaveException e) {
+
+        log.error("로그 저장 실패: {}", e.getMessage(), e);
+
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("저장에 실패했습니다.");
+    }
+
+    // 에상치 못한 예외처리
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception e) {
+        log.error("서버 내부 예외 발생", e);
+
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("서버 오류가 발생했습니다.");
     }
 }
