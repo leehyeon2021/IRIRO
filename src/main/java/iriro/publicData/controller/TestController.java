9ce4b897b9be715e1defef6df5f1d17d7a4f84cd
leehyeon2021 <leehyeon2021@gmail.com>
2026-03-31 17:42:30 +0900

    260331 어노테이션 수정

--- src/main/java/iriro/publicData/controller/TestController.java
+++ src/main/java/iriro/publicData/controller/TestController.java
@@ -9,8 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequiredArgsConstructor
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api")
 public class TestController {
     private final CrimeRoadFetchService cf;
