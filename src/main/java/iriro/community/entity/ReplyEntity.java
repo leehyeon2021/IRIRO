b3a93a5068fa5069cf8b9c5e9541d7f0985d37fe
so0hub <so0mile02@gmail.com>
2026-03-31 16:42:31 +0900

    ReplyEntity 수정

--- src/main/java/iriro/community/entity/ReplyEntity.java
+++ src/main/java/iriro/community/entity/ReplyEntity.java
@@ -19,9 +19,6 @@ public class ReplyEntity extends BaseTime {
     @GeneratedValue( strategy = GenerationType.IDENTITY )
     private Integer replyId;
 
-    @Column( columnDefinition = "longtext" )
-    private String replyContent;
-
     // 회원번호(FK)
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn( name = "user_id")
@@ -32,6 +29,12 @@ public class ReplyEntity extends BaseTime {
     @JoinColumn( name = "board_id")
     private BoardEntity boardEntity;
 
+    @Column( columnDefinition = "longtext" )
+    private String nickname;
+
+
+    @Column( columnDefinition = "longtext" )
+    private String replyContent;
 
     // Entity --> Dto 변환
     public ReplyDto toDto(){
@@ -39,6 +42,7 @@ public class ReplyEntity extends BaseTime {
                 .replyId(this.replyId)
                 .userId(this.getUserEntity().getUserId())
                 .boardId(this.getBoardEntity().getBoardId())
+                .nickname(this.nickname)
                 .replyContent(this.replyContent)
                 .createdAt(this.getCreatedAt().toString())
                 .updatedAt(this.getUpdatedAt().toString())
