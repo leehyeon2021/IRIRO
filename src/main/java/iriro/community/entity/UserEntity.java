f51ff04e8e1d168a8c594f2198be3ea55460fa8a
so0hub <so0mile02@gmail.com>
2026-04-01 17:04:46 +0900

    toDto 수정

--- src/main/java/iriro/community/entity/UserEntity.java
+++ src/main/java/iriro/community/entity/UserEntity.java
@@ -8,6 +8,8 @@ import lombok.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import static ch.qos.logback.classic.spi.ThrowableProxyVO.build;
+
 
 @Entity
 @Table( name = "users")
@@ -40,8 +42,10 @@ public class UserEntity extends BaseTime {
     private List<BoardEntity> boardList = new ArrayList<>();
 
     // 댓글
-    @OneToMany(mappedBy = "")
-    private List<ReplyEntity> replies;
+    @OneToMany(mappedBy = "userEntity",cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @Builder.Default
+    @ToString.Exclude
+    private List<ReplyEntity> replies = new ArrayList<>();
 
 
     // Entity --> Dto 변환함수 // 생고기에서 플레이팅 접시용으로 바꾸는 거야~
@@ -53,6 +57,17 @@ public class UserEntity extends BaseTime {
                 .nickName(this.nickname)
                 .createAt( this.getCreatedAt().toString())
                 .updateAt( this.getUpdatedAt().toString())
+                .myBoards(this.boardList != null ?
+                        this.boardList.stream()
+                                .map(board->board.toDto())
+                                .toList()
+                        : new ArrayList<>())
+                .myReplies(this.replies != null ?
+                        this.replies.stream()
+                        .map(reply->reply.toDto())
+                        .toList()
+                : new ArrayList<>())
+
                 .build();
     }
 
