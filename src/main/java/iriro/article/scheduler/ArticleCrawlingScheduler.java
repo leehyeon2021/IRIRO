b8febc8203a24440b5ed7b102599cf754423dcd3
leehyeon2021 <leehyeon2021@gmail.com>
2026-04-04 15:30:09 +0900

    260404 [크롤링] 재미나이 잘가라

--- src/main/java/iriro/article/scheduler/ArticleCrawlingScheduler.java
+++ src/main/java/iriro/article/scheduler/ArticleCrawlingScheduler.java
@@ -20,7 +20,7 @@ public class ArticleCrawlingScheduler {
         // 2. 지역구 하나씩 검색
         for (String district : RegionConstants.seoulDistricts) {
 
-            String keyword = district + " 범죄";
+            String keyword = district + " 묻지마";
 
             // 1. 노컷뉴스 크롤링 (Selenium 사용)
             System.out.println("[노컷뉴스] 크롤링 시작");
